dc69125474b0bf16474373a1cdefabba68f32ad4
Habaru <64592358+HabaruWasTaken@users.noreply.github.com>
2021-08-21 11:00:15 +0000

    fixed some bug

--- script.js
+++ script.js
@@ -5,10 +5,55 @@ $(document).ready(function(){
       } else {
         $(".navbar").removeClass("sticky");
       }
+
+      if (this.scrollY > 500){
+        if ($(".menu-btn i").hasClass("active")) return;
+        $(".scroll-up-btn").addClass("show");
+        $(".scroll-up-btn").addClass("active");
+      } else {
+        $(".scroll-up-btn").removeClass("show");
+        $(".scroll-up-btn").removeClass("active");
+      }
+  })
+
+  $(".scroll-up-btn").click(function(){
+    $('html').animate({ scrollTop: 0 })
+  })
+
+  var typed = new Typed(".typing", {
+    strings: ['Student', 'Developer', 'Freelancer'],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  })
+
+  var typed = new Typed(".typing-2", {
+    strings: ['Student', 'Developer', 'Freelancer'],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
   })
 
   $('.menu-btn').click(function(){
     $('.navbar .menu').toggleClass("active")
     $('.menu-btn i').toggleClass("active")
+    
+    if ($(".scroll-up-btn").hasClass("active")){
+
+      if ($(".scroll-up-btn").hasClass("show")){
+        if (!$('.navbar .menu').hasClass("active")) return
+        $(".scroll-up-btn").removeClass("show");
+        console.log('remove')
+      } else {
+        if ($('.navbar .menu').hasClass("active")) return
+        $(".scroll-up-btn").addClass("show");
+        console.log('add')
+      }
+    }
+  })
+  
+  $(".navbar .menu li").click(()=>{
+    $('.navbar .menu').toggleClass('active')
+    $('.menu-btn i'). toggleClass('active')
   })
 });
